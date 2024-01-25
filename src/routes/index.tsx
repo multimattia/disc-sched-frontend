ae641935db302878dbd3d7509883bae9955868de
maat <matt@mattchan.xyz>
2024-01-25 16:32:19 -0500

    Fix infinite loop caused by clerk, replace with conditional rendering of link to sign in page.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,37 @@
 import { MessageForm } from "@/components/MessageForm";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { useUser } from "@clerk/clerk-react";
+import { SignedOut } from "@clerk/clerk-react";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: ({ context }) => {
-    if (!context.auth.isSignedIn) {
-      throw redirect({
-        to: "/sign-in",
-      });
-    }
-  },
   component: HomeComponent,
 });
 
 function HomeComponent() {
-  const { user, isLoaded } = useUser();
+  const { isSignedIn, user } = useUser();
   return (
     <div className="flex flex-col max-w-screen-lg m-auto">
       <h1 className="text-slate-800 text-5xl font-bold my-5">
         Discord message sending test
       </h1>
-      {isLoaded ? <p> Hi, {user!.fullName}!</p> : ""}
-      <MessageForm />
+      {isSignedIn ? (
+        <>
+          <p> Hi, {user!.fullName}!</p>
+          <MessageForm />
+        </>
+      ) : (
+        <span>
+          <SignedOut>
+            <Link
+              to="/sign-in"
+              className="text-red-500 cursor-pointer hover:underline"
+            >
+              Please sign in.
+            </Link>
+          </SignedOut>
+        </span>
+      )}
     </div>
   );
 }
